266d3b7b23871858f1c9ac21a2f9fbf625ce1e6f
Brian Kurilko <briankurilko@gmail.com>
2024-04-10 14:16:02 -0400

    Started randomly generating buildIds so to not run into race conditions when running tests

--- tests/submissions.sh
+++ tests/submissions.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Submission should complete successfully.
-buildId="opinionated-pipeline-test"
+random=$(echo $RANDOM | md5sum | head -c 8)
+buildId="opinionated-pipeline-test-$random"
 gcloud builds submit . --config cloudbuild.yaml --substitutions "_GIT_CLONE_URL=https://github.com/brianpipeline/test-cloudbuild.git,_GIT_REPOSITORY_NAME="test-cloudbuild",_GIT_REF="refs/heads/main",_GIT_HEAD_SHA="d4828ea0e1bca17e8f6a4cc387d5bbaf33714566",_SHORT_BUILD_ID=$buildId" --region=us-central1
 
 # Submission should fail and downstream pipeline should receive a 'Pipeline failed', and delete the pipeline accordingly.
-buildId="opinionated-pipeline-test"
+random=$(echo $RANDOM | md5sum | head -c 8)
+buildId="opinionated-pipeline-test-$random"
 if gcloud builds submit . --config cloudbuild.yaml --substitutions "_GIT_CLONE_URL=https://github.com/brianpipeline/test-cloudbuild-failure.git,_GIT_REPOSITORY_NAME="test-cloudbuild-failure",_GIT_REF="refs/heads/main",_GIT_HEAD_SHA="b3fa043e677500882e689fc1a978d96056d6702d",_SHORT_BUILD_ID=$buildId" --region=us-central1; then
     exit 1
 fi
