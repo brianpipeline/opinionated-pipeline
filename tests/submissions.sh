f5eebb50aed35a2d63109abf662df13ec44f53fa
Brian Kurilko <briankurilko@gmail.com>
2024-04-10 11:25:44 -0400

    Updated pipeline test

--- tests/submissions.sh
+++ tests/submissions.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcloud builds submit . --config cloudbuild.yaml --substitutions "_REPO_TO_CLONE=https://github.com/brianpipeline/test-cloudbuild.git,_REPO_NAME="test-cloudbuild",_SHORT_BUILD_ID=test_build_id" --region=us-central1
-
+# Submission should should complete successfully.
+buildId="opinionated-pipeline-test"
+gcloud builds submit . --config cloudbuild.yaml --substitutions "_REPO_TO_CLONE=https://github.com/brianpipeline/test-cloudbuild.git,_REPO_NAME="test-cloudbuild",_SHORT_BUILD_ID=$buildId" --region=us-central1
