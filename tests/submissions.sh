9bf8339427b90059a34eb342055751aa54ab8239
Brian Kurilko <briankurilko@gmail.com>
2024-04-10 12:09:54 -0400

    Reversed if statement, now looking to see that topic was deleted

--- tests/submissions.sh
+++ tests/submissions.sh
@@ -8,7 +8,12 @@ buildId="opinionated-pipeline-test"
 if gcloud builds submit . --config cloudbuild.yaml --substitutions "_REPO_TO_CLONE=https://github.com/brianpipeline/test-cloudbuild-failure.git,_REPO_NAME="test-cloudbuild-failure",_SHORT_BUILD_ID=$buildId" --region=us-central1; then
     exit 1
 fi
-if ! gcloud pubsub subscriptions describe "subscription_$buildId"; then
+if gcloud pubsub subscriptions describe "subscription_$buildId"; then
     echo "Subscription was not deleted"
+    exit 1
+fi
+
+if gcloud pubsub topics describe "topic_$buildId"; then
+    echo "Topic was not deleted"
     exit 1
 fi
