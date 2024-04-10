c657605bcf131a46f5b9dad4ad591ca6ee202d5d
Brian Kurilko <briankurilko@gmail.com>
2024-04-10 09:32:07 -0400

    Removed escaped quotes around build id, its messing up the build

--- tests/submissions.sh
+++ tests/submissions.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud builds submit . --config cloudbuild.yaml --substitutions "_REPO_TO_CLONE=https://github.com/brianpipeline/test-cloudbuild.git,_REPO_NAME="test-cloudbuild",_SHORT_BUILD_ID=\"test_build_id\"" --region=us-central1
+gcloud builds submit . --config cloudbuild.yaml --substitutions "_REPO_TO_CLONE=https://github.com/brianpipeline/test-cloudbuild.git,_REPO_NAME="test-cloudbuild",_SHORT_BUILD_ID=test_build_id" --region=us-central1
