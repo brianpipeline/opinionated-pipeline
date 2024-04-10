52f14155c01f59670f61965cca2014e95d006fba
Brian Kurilko <briankurilko@gmail.com>
2024-04-10 11:58:15 -0400

    Trying to pull a message to verify that a message is being sent (hopefully without acking)

--- tests/submissions.sh
+++ tests/submissions.sh
@@ -8,3 +8,8 @@ buildId="opinionated-pipeline-test"
 if gcloud builds submit . --config cloudbuild.yaml --substitutions "_REPO_TO_CLONE=https://github.com/brianpipeline/test-cloudbuild-failure.git,_REPO_NAME="test-cloudbuild-failure",_SHORT_BUILD_ID=$buildId" --region=us-central1; then
     exit 1
 fi
+message=$(gcloud pubsub subscriptions pull "subscription_$buildId" --format='value(message.data)')
+if [[ $message != "Pipeline failed." ]]; then
+    echo "Wrong message was sent to the downstream pipeline."
+    exit 1
+fi
