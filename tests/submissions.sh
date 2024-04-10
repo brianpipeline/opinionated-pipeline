1b1c6389771a35a179d8e5a2d3b239b97625d75a
Brian Kurilko <briankurilko@gmail.com>
2024-04-10 09:34:16 -0400

    Whitespace

--- tests/submissions.sh
+++ tests/submissions.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 gcloud builds submit . --config cloudbuild.yaml --substitutions "_REPO_TO_CLONE=https://github.com/brianpipeline/test-cloudbuild.git,_REPO_NAME="test-cloudbuild",_SHORT_BUILD_ID=test_build_id" --region=us-central1
+
